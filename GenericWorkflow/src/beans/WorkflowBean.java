c38aac094e5814aba8c9c014a5a1c52ca0b0b046
jadkaady <jadkaady@gmail.com>
2013-09-23 19:41:55 +0300

    Workflow Bean

--- GenericWorkflow/src/beans/WorkflowBean.java
+++ GenericWorkflow/src/beans/WorkflowBean.java
@@ -1,36 +1,52 @@
 package beans;
 
 import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.faces.bean.ManagedBean;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import context.AppContext;
+
+import dao.IWorkflowDao;
+import domain.Workflow;
 
 @Component("workflowBean")
 @Scope
 @ManagedBean
 public class WorkflowBean implements Serializable{
-
-	private String name;
-
-    private String description;
-
-	public String getName() {
-		return name;
+	
+	private IWorkflowDao workflowDao;
+	private Workflow workflow = new Workflow();
+	
+	public WorkflowBean() {
+		
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public Workflow getWorkflow() {
+		return workflow;
 	}
 
-
-	public String getDescription() {
-		return description;
+	public void setWorkflow(Workflow workflow) {
+		this.workflow = workflow;
 	}
-
-	public void setDescription(String description) {
-		this.description = description;
+	
+	public void submit(){
+		workflowDao = (IWorkflowDao) AppContext.getBean("workflowDao");
+		workflowDao.saveOrUpdate(workflow);
+	}
+	
+	public void startWorkflow(String workflowName) throws SecurityException, IllegalArgumentException, NoSuchMethodException, ClassNotFoundException, IllegalAccessException, InvocationTargetException, InstantiationException{
+		workflowDao = (IWorkflowDao) AppContext.getBean("workflowDao");
+		workflowDao.changeState(workflowName, 0, "Submit", null, "jad", "no comments");
+	}
+	
+	public void changeState(String workflowName, Integer workflowInstanceId, String actionName) throws SecurityException, IllegalArgumentException, NoSuchMethodException, ClassNotFoundException, IllegalAccessException, InvocationTargetException, InstantiationException{
+		workflowDao = (IWorkflowDao) AppContext.getBean("workflowDao");
+		workflowDao.changeState(workflowName, workflowInstanceId, actionName, null, "employee 1", "all good");
 	}
 	    
 }
