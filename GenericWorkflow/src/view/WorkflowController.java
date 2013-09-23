9e3f634350ec28d4a4594372bc5c74ca209b8137
elienasr <elienasr87@gmail.com>
2013-09-23 19:36:59 +0300

    workflow jsf

---
+++ GenericWorkflow/src/view/WorkflowController.java
@@ -0,0 +1,27 @@
+package view;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import dao.IWorkflowDao;
+import domain.Workflow;
+
+@Controller
+public class WorkflowController {
+	
+	@Autowired
+	private IWorkflowDao workflowDao;
+
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "/listworkflows", method = RequestMethod.GET)
+	public String show(Model model) {
+		List<Workflow> workflows = (List<Workflow>) workflowDao.findAll(Workflow.class);
+		model.addAttribute("workflows",workflows);
+		return "listworkflows";
+	}
+}
