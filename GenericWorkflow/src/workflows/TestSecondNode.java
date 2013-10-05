9912d90f2356cf85aa97cb1a56b4b2c7888b7697
jadkaady <jadkaady@gmail.com>
2013-10-05 18:48:13 +0300

    Test class for node

--- GenericWorkflow/src/workflows/TestSecondNode.java
+++ GenericWorkflow/src/workflows/TestSecondNode.java
@@ -2,4 +2,15 @@ package workflows;
 
 public class TestSecondNode {
 
+	public void AcceptPapers(){
+		//do logic here
+		String test = "";
+		test = "hello world";
+	}
+	
+	public void RejectPapers(){
+		//do logic here
+		String test = "";
+		test = "hello world";
+	}
 }
